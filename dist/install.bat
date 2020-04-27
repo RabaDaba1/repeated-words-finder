1a796e183a4e0070428668f3e13ac0648f577839
Kacper Rabczewski <krabczewski@gmail.com>
2020-04-27 20:57:26 +0200

    remove star.bat shortcut, add install.bat

---
+++ dist/install.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
+
+echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\text.lnk" >> %SCRIPT%
+echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
+echo oLink.TargetPath = "%CD%/index.html" >> %SCRIPT%
+echo oLink.Save >> %SCRIPT%
+
+cscript /nologo %SCRIPT%
+del %SCRIPT%

d6c29d6807d75b048e135269a58bc72f4301575f
Kacper Rabczewski <59542220+RabaDaba1@users.noreply.github.com>
2020-04-27 21:53:26 +0200

    delete polish letters from install.bat

--- install.bat
+++ install.bat
@@ -3,7 +3,7 @@ set SCRIPT="%TEMP%\%RANDOM%-%RANDOM%-%RANDOM%-%RANDOM%.vbs"
 
 echo Set oWS = WScript.CreateObject("WScript.Shell") >> %SCRIPT%
 echo sLinkFile = "%USERPROFILE%\Desktop\text.lnk" >> %SCRIPT%
-echo sLinkFile = "%USERPROFILE%\Desktop\PowtórzoneSlowa.lnk" >> %SCRIPT%
+echo sLinkFile = "%USERPROFILE%\Desktop\PowtorzoneSlowa.lnk" >> %SCRIPT%
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> %SCRIPT%
 echo oLink.TargetPath = "%CD%/dist/index.html" >> %SCRIPT%
 echo oLink.Save >> %SCRIPT%
